3cce8bd4e7225f45e314660b2694a18e478d0898
Jose Zapata Zambrano <joferzz1989@gmail.com>
2018-09-03 15:28:39 -0400

    avance

--- src/app/components/monedero/monedero.component.ts
+++ src/app/components/monedero/monedero.component.ts
@@ -30,7 +30,7 @@ export class MonederoComponent implements OnInit {
 
         resultados = this._cajero.calcular(denominaciones);
 
-        console.log('denominaciones: ', denominaciones);
+        console.log(`monto: ${denominaciones.monto} monedas: (${denominaciones.denominaciones.join(', ')})`);
         console.log('respuesta: ', resultados);
       });
 
